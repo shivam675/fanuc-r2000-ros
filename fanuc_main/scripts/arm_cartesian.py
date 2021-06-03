93e36ecd6d2740148b26af87b7f34690cab2a889
shivam675 <shivam31199@gmail.com>
2021-06-03 15:24:04 +0530

    fanuc simulated entire

--- fanuc_main/scripts/arm_cartesian.py
+++ fanuc_main/scripts/arm_cartesian.py
@@ -95,89 +95,89 @@ def main():
 
     waypoints = []
 
-    x1, y1 = 0.86, 0.19
-    x2, y2 = 0.72, 0.19
-    x3, y3 = 0.72, -0.19
-    x4, y4 = 0.86, -0.19
+    x1, y1 = 1.82, 0.37
+    x2, y2 = 2.26, 0.37
+    x3, y3 = 2.26, -0.37
+    x4, y4 = 1.82, -0.37
     resolution = 1
 
     first_quad = geometry_msgs.msg.Pose()
-    first_quad.position.x = 0.929 #x1
-    first_quad.position.y = 0.0 #y1
-    first_quad.position.z = 0.649
-    first_quad.orientation.x = 0.0
-    first_quad.orientation.y = 0.0
-    first_quad.orientation.z = -0.707
-    first_quad.orientation.w = 0.707
+    first_quad.position.x = 1.828
+    first_quad.position.y = 0.37
+    first_quad.position.z = 1.05
+    first_quad.orientation.x = 0.002
+    first_quad.orientation.y = 0.710
+    first_quad.orientation.z = -0.01
+    first_quad.orientation.w = 0.703
     waypoints.append(copy.deepcopy(first_quad)) # Init pose
 
-    first_quad.position.x -= 0.196
-    waypoints.append(copy.deepcopy(first_quad)) # step1: back
+    # first_quad.position.x -= 0.196
+    # waypoints.append(copy.deepcopy(first_quad)) # step1: back
 
-    first_quad.position.z -= 0.1
-    waypoints.append(copy.deepcopy(first_quad)) #step2: down
+    # first_quad.position.z -= 0.1
+    # waypoints.append(copy.deepcopy(first_quad)) #step2: down
 
-    first_quad.position.y += 0.183
-    waypoints.append(copy.deepcopy(first_quad)) # step3: (x,y): 0,0
+    # first_quad.position.y += 0.183
+    # waypoints.append(copy.deepcopy(first_quad)) # step3: (x,y): 0,0
 
-    first_quad.position.x += 0.121
-    waypoints.append(copy.deepcopy(first_quad)) # step4: (x,y): 0,1
+    # first_quad.position.x += 0.121
+    # waypoints.append(copy.deepcopy(first_quad)) # step4: (x,y): 0,1
 
-    first_quad.position.y -= 0.385
-    waypoints.append(copy.deepcopy(first_quad)) # step5: (x,y): 1,1
+    # first_quad.position.y -= 0.385
+    # waypoints.append(copy.deepcopy(first_quad)) # step5: (x,y): 1,1
 
-    first_quad.position.x -= 0.121
-    waypoints.append(copy.deepcopy(first_quad)) # step6: (x,y): 1,0
+    # first_quad.position.x -= 0.121
+    # waypoints.append(copy.deepcopy(first_quad)) # step6: (x,y): 1,0
 
-    aRm.go_to_pose(waypoints)
+    # aRm.go_to_pose(waypoints)
 
-    # second_quad = geometry_msgs.msg.Pose()
-    # second_quad.position.x = x2
-    # second_quad.position.y = y2
-    # second_quad.position.z = 0.649
-    # second_quad.orientation.x = 0.0
-    # second_quad.orientation.y = 0.0
-    # second_quad.orientation.z = -0.707
-    # second_quad.orientation.w = 0.707
-    # waypoints.append(copy.deepcopy(second_quad))
+    second_quad = geometry_msgs.msg.Pose()
+    second_quad.position.x = x2
+    second_quad.position.y = y2
+    second_quad.position.z = 1.05
+    second_quad.orientation.x = 0.002
+    second_quad.orientation.y = 0.710
+    second_quad.orientation.z = -0.01
+    second_quad.orientation.w = 0.703
+    waypoints.append(copy.deepcopy(second_quad))
     
-    # l = int((y2-y3)*100)
-    # b = int((x1-x2)*100)
-    # stride = 3
-    # for i in range(1,l+1,stride):
-    #     middle_waypoints_one = geometry_msgs.msg.Pose()
-    #     middle_waypoints_one.position.x = x1
-    #     middle_waypoints_one.position.y = y1 - i/100
-    #     middle_waypoints_one.position.z = 0.649
-    #     middle_waypoints_one.orientation.x = 0.0
-    #     middle_waypoints_one.orientation.y = 0.0
-    #     middle_waypoints_one.orientation.z = -0.707
-    #     middle_waypoints_one.orientation.w = 0.707
-    #     waypoints.append(copy.deepcopy(middle_waypoints_one))
+    l = int((y2-y3)*100)
+    b = int((x1-x2)*100)
+    stride = 3
+    for i in range(1,l+1,stride):
+        middle_waypoints_one = geometry_msgs.msg.Pose()
+        middle_waypoints_one.position.x = x1
+        middle_waypoints_one.position.y = y1 - i/100
+        middle_waypoints_one.position.z = 1.05
+        middle_waypoints_one.orientation.x = 0.002
+        middle_waypoints_one.orientation.y = 0.710
+        middle_waypoints_one.orientation.z = -0.01
+        middle_waypoints_one.orientation.w = 0.703
+        waypoints.append(copy.deepcopy(middle_waypoints_one))
 
-    #     middle_waypoints_two = geometry_msgs.msg.Pose()
-    #     middle_waypoints_two.position.x = x2
-    #     middle_waypoints_two.position.y = y1 - i/100
-    #     middle_waypoints_two.position.z = 0.649
-    #     middle_waypoints_two.orientation.x = 0.0
-    #     middle_waypoints_two.orientation.y = 0.0
-    #     middle_waypoints_two.orientation.z = -0.707
-    #     middle_waypoints_two.orientation.w = 0.707
-    #     waypoints.append(copy.deepcopy(middle_waypoints_two))
+        middle_waypoints_two = geometry_msgs.msg.Pose()
+        middle_waypoints_two.position.x = x2
+        middle_waypoints_two.position.y = y1 - i/100
+        middle_waypoints_two.position.z = 1.05
+        middle_waypoints_two.orientation.x = 0.002
+        middle_waypoints_two.orientation.y = 0.710
+        middle_waypoints_two.orientation.z = -0.01
+        middle_waypoints_two.orientation.w = 0.703
+        waypoints.append(copy.deepcopy(middle_waypoints_two))
     
     target_poses = []
 
     while not rospy.is_shutdown():
-        target_poses.append(copy.deepcopy(waypoints[3]))
-        target_poses.append(copy.deepcopy(waypoints[4]))
-        target_poses.append(copy.deepcopy(waypoints[5]))
-        target_poses.append(copy.deepcopy(waypoints[6]))
-        aRm.go_to_pose(target_poses)
-        target_poses = []
+        # target_poses.append(copy.deepcopy(waypoints[3]))
+        # target_poses.append(copy.deepcopy(waypoints[4]))
+        # target_poses.append(copy.deepcopy(waypoints[5]))
+        # target_poses.append(copy.deepcopy(waypoints[6]))
+        # aRm.go_to_pose(target_poses)
+        # target_poses = []
 
-    # print(waypoints[0:4])
-    # print(len(waypoints))
-    # aRm.go_to_pose(waypoints) 
+        print(waypoints[0:4])
+        print(len(waypoints))
+        aRm.go_to_pose(waypoints) 
 
     del aRm   
 
